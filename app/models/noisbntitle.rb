40cf3a420ce2bfd6f0dd433941959213ee4ebcb9
akhilesh <kataria.subs@gmail.com>
2012-05-05 12:40:20 +0530

    Ability to create core list for titles without isbn

--- app/models/noisbntitle.rb
+++ app/models/noisbntitle.rb
@@ -4,6 +4,8 @@ class Noisbntitle < ActiveRecord::Base
   belongs_to :publisher
   belongs_to :category
   belongs_to :jbtitle, :foreign_key => "title_id", :class_name => "Title"
+  belongs_to :jbcategory, :foreign_key => 'category_id', :class_name => "Category"
+  
   
   has_attached_file :cover, :styles => {:thumb => "100x100>", :medium => "200x200>"}, 
   :path => ':style/:id.:extension', :default_url => "/images/missing_:style.jpg"
@@ -25,7 +27,9 @@ class Noisbntitle < ActiveRecord::Base
   validates_attachment_size :cover, :less_than => 50.kilobytes, :message => 'file size maximum 50 KB allowed'
   validates_attachment_content_type :cover, :content_type => ['image/jpeg']
   
-  before_create :set_defaults_on_create
+  before_create :upsert_legacy_title
+  
+  before_validation :set_defaults_on_create
 
   def self.new_from_title(legacytitleid)
     nt = new
@@ -44,7 +48,31 @@ class Noisbntitle < ActiveRecord::Base
   def set_defaults_on_create
     self.verified = 'N'
     self.enriched = 'N'
+    self.publisher_name = self.publisher.name unless self.publisher.nil?
   end
-    
   
+  def upsert_legacy_title
+    attributes = {
+      :title => self.title,
+      :authorid => Author.find_or_create_by_firstname(self.author).id,
+      :publisherid => (self.publisher.id unless self.publisher.nil?),
+      :isbn_10 => 'NOISBN',
+      :isbn_13 => 'NOISBN',
+      :category => self.jbcategory,
+      :language => self.language,
+      :titletype => 'B',
+      :insertdate => Time.zone.now,
+      :userid => 'AMS',
+      :flag_isbn_image => ('Y' unless cover.nil?),
+      :mrp => self.listprice,
+      :yearofpublication => self.pub_year,
+      :no_of_pages => self.page_cnt,
+      :format => self.format
+    }
+    if self.jbtitle.nil?
+      self.jbtitle = Title.create!(attributes)
+    else
+      self.jbtitle.update_attributes!(attributes)
+    end    
+  end  
 end
