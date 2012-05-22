4c3ea45edfc7b705c918bc44a89fe952f741c50e
akhilesh <kataria.subs@gmail.com>
2012-05-22 16:05:32 +0530

    new: added language filter for extracting sap files

--- app/controllers/enrichedtitles_controller.rb
+++ app/controllers/enrichedtitles_controller.rb
@@ -14,7 +14,7 @@ class EnrichedtitlesController < ApplicationController
     end
 
     unless params[:file_id].blank?
-      send_data SAP::create_file(Enrichedtitle, params[:file_id]),
+      send_data SAP::create_file(Enrichedtitle, params),
           :type => 'text/text; charset=iso-8859-1; header=present',
           :disposition => "attachment; filename=#{params[:file_id]}.txt"
     end    
