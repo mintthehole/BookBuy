fcbdc8fb1ec8e5177f91ad2e5cd53ed1ee3736b9
akhilesh <kataria.subs@gmail.com>
2012-05-23 17:08:01 +0530

    improvement: speed up outbound delivery creation

--- app/controllers/outbound_deliveries_controller.rb
+++ app/controllers/outbound_deliveries_controller.rb
@@ -17,8 +17,17 @@ class OutboundDeliveriesController < ApplicationController
   
   def create
     @form_id = params[:outbound_delivery_id]
-    @outbound_delivery = OutboundDelivery.build_from_params( params[:outbound_delivery] )
-    flash[:notice] = "Outbound Delivery successfully created" if @outbound_delivery.save
-    respond_with( @outbound_delivery, :layout => !request.xhr? )    
+    outbound_delivery = OutboundDelivery.find_or_create_by_delivery_no_and_sto_no_and_destination_plant(params[:delivery_no],params[:sto_no], params[:destination_plant])
+    @errors = outbound_delivery.errors unless outbound_delivery.valid?
+    
+    if outbound_delivery.valid?
+      material_code = params[:material_code]
+      book_no = params[:book_no]
+      outbound_delivery_id = outbound_delivery.id
+      outbound_delivery_item = OutboundDeliveryItem.find_or_create_by_book_no(:book_no => book_no, :material_code => material_code, :outbound_delivery_id => outbound_delivery_id)
+      @errors = outbound_delivery_items.errors unless outbound_delivery_item.valid?
+    end
+
+    respond_with( @errors, :layout => !request.xhr? )    
   end
 end
